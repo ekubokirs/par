1cdd6c85c45dc4d11cdc360965736d72314c3b3a
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-09-18 16:25:55 -0700

    updated registration and user controllers

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,9 +1,43 @@
 class RegistrationController < ApplicationController
+	after_action	:clear_expired_registrants
+	before_action	:get_registrant
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.new user_params
+		@user.save
+
+		redirect_to root_url
+		flash[:notice] = "Registration Completed!"
 	end
 	
+	private
+
+	def clear_expired_registrants
+		Registrant.destroy_all(:expires_at.lt => Time.now)
+	end
+
+	def get_registrant
+		@registrant = Registrant.find_by_code params[:code]
+		unless @registrant
+			Registrant.where(expires_at.lt => Time.now)
+			redirect_to login_url
+			flash[:alert] = "Registration Link Expired"
+		end
+	end
+
+	def user_params
+		params.require(:user).permit(
+			:first_name,
+			:last_name,
+			:email,
+			:password,
+			:password_confirmation,
+			:birthday,
+			:bio
+		)
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 	
-	def index
-	end
-
 	def new
 	end
 
 	def create
+	end
+
+	def show
 	end
 
 	def edit
