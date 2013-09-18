b85d2b826a3cc4dd601db3fd1bd08e7d74d82e20
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-09-18 12:20:02 -0700

    set up initial rails app without database or rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Par::Application.config.secret_key_base = '56be803f22926838328dd1abc8dfce7ec8c140758339fcee1d2369ddbc116503b717e170f502972d17c6854fd57d0de7e2357b36d1a878d23ba95726f148c4a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Par::Application.config.session_store :cookie_store, key: '_par_session'
