b8f15d6118e90c963cc506daf4265cdcbf8bb852
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-09-18 15:51:00 -0700

    created all routes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,6 @@
 class SessionController < ApplicationController
+	before_action :is_authenticated, only: [:homepage]
+	
 	def homepage
 	end
 	
@@ -6,13 +8,45 @@ class SessionController < ApplicationController
 	end
 
 	def create
-	end
+		@user = User.find_by_email params[:email]
+    if params[:email].blank?
+      flash[:alert] = "Please Enter E-mail"
+      render :new
+    else
+      if @user
+        if params[:password].blank?
+          @user.code = SecureRandom.urlsafe_base64
+          @user.expires_at = Time.now + 4.hours
+          @user.save
 
-	def edit
+          PasswordMailer.reset_email(@user).deliver
+          redirect_to login_url
+          flash[:alert] = "Password Reset E-mail Sent!"
+        else
+          if @user.authenticate(params[:password])
+            session[:user_id] = @user._id
+            redirect_to root_url
+          else
+            redirect_to login_url
+            flash[:alert] = "Wrong E-mail or Password.  Please Try Again!"
+          end
+        end
+      else
+        @registrant = Registrant.new(email: params[:email])
+        @registrant.save
+
+        PasswordMailer.registration_email(@registrant).deliver
+
+        redirect_to login_url
+        flash[:alert] = "A Registration E-mail Has Been Sent!"
+      end
+    end
 	end
 
 	def destroy
+		session[:user_id] = nil
+		redirect_to login_url
+		flash[:alert] = "You've Logged Out!"
 	end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Par::Application.routes.draw do
+
+  resources :events
+  resources :users
+
+  #Registration
+  get   'register/:code'  => 'registration#new', as: :register
+  post  'register/:code'  => 'registration#create'
+
+  #Login
   get 'login' =>  'session#new'
   get 'login' =>  'session#create'
+
+  #Logout
+  get    'logout' => 'session#destroy'
+  delete 'logout' => 'session#destroy'
+
+  #Password Reset
+  get   'reset/:code' =>  'password#edit', as: :reset
+  put   'reset/:code' =>  'password#update'
+  patch 'reset/:code' =>  'password#update'
+
+  root 'session#homepage'
+
 end
