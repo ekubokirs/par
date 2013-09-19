ce249a2e7fdd68b5435f3212376f608156963c41
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-09-18 17:58:03 -0700

    wrote password controller, finished the users controller, updated event model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ApplicationController
-	
-	def new
-	end
-
-	def create
-	end
 
 	def show
+		@user = User.find(params[:id])
 	end
 
 	def edit
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event
   include MongoMapper::Document
 
+  belongs_to	:user
+
   key :title,				String
   key :venue,				String
   key :event_type,	String
