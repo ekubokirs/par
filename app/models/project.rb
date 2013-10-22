c1c596d0f3e7f8dc07aa9c1bcd1d3417ceca07e1
Emilia Kirschenbaum <ekubokirs@gmail.com>
2013-10-21 17:37:09 -0700

    started projects controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,11 @@
 class Project
   include MongoMapper::Document
 
+  key :supply, 				String
+  key :direction,			String
+  key	:prep_time,			String
+  key	:total_time,		String
+  key	:links,					String
+  key	:project_title,	String
 
 end
